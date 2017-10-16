c6683fc50dd937b361fd5304d12d75487735c35e
marvinjason <marvinjason.sy@gmail.com>
2017-10-17 00:00:31 +0800

    Update controllers

--- controllers.py
+++ controllers.py
@@ -6,13 +6,14 @@ from uuid import uuid4
 def is_authenticated():
     access_token = request.args.get('access_token')
     if access_token is not None:
-        count = Session.query().filter(access_token=access_token).count()
+        count = Session.query(Session.access_token == access_token).count()
         if count > 0:
             return True
     return False
 
 users = Blueprint('users', __name__)
-@users.route('/users/create', methods=['POST'])
+
+@users.route('/users', methods=['POST'])
 def create():
     user = User()
     user.last_name = request.form['last_name']
@@ -21,7 +22,6 @@ def create():
     user.password = bcrypt.hashpw(request.form['password'], bcrypt.gensalt())
     user.put()
     session = Session(access_token=str(uuid4()), user=user.key).put()
-
     return jsonify({
         'id': user.key.id(),
         'last_name': user.last_name,
@@ -30,42 +30,41 @@ def create():
         'access_token': session.get().access_token
     })
 
-@users.route('/users/update', methods=['PUT'])
-def update():
-    if is_authenticated():
-        pass
-    else:
-        return jsonify({'error': {'status': 401, 'message': 'unauthorized'}}), 401
+@users.route('/users/<id>', methods=['GET'])
+def show(id):
+    user = User.get_by_id(int(id))
+    return jsonify({
+        'last_name': user.last_name,
+        'first_name': user.first_name,
+        'email': user.email
+    })
 
-@users.route('/users/destroy', methods=['DELETE'])
-def destroy():
+@users.route('/users/<id>', methods=['PUT'])
+def update(id):
     if is_authenticated():
         access_token = request.args.get('access_token')
-        session = Session.query().filter(access_token=access_token).fetch()[0]
-        user = session.user.get()
-        response = {
-            'id': user.key.id(),
-            'last_name': user.last_name,
-            'first_name': user.first_name,
-            'email': user.email,
-            'access_token': session.get().access_token
-        }
-        user.key.delete()
-        return jsonify(response)
-    else:
-        return jsonify({'error': {'status': 401, 'message': 'unauthorized'}}), 401
+        session = Session.query(Session.access_token == access_token).fetch()[0]
 
-@users.route('/users/<id>')
-def show(id):
-    user = User.query().filter(User.key == User.make_key(id)).count()
-    return str(user)
-    # return jsonify({
-    #     'last_name': user.last_name,
-    #     'first_name': user.first_name,
-    #     'email': user.email
-    # })
+        if session.user.id() == int(id):
+            pass
 
-sessions = Blueprint('sessions', __name__)
-@sessions.route('/sessions', methods=['POST'])
-def create():
-    pass
+    return jsonify({'error': {'status': 401, 'message': 'unauthorized'}}), 401
+
+@users.route('/users/<id>', methods=['DELETE'])
+def destroy(id):
+    if is_authenticated():
+        access_token = request.args.get('access_token')
+        session = Session.query(Session.access_token == access_token).fetch()[0]
+
+        if session.user.id() == int(id):
+            user = session.user.get()
+            response = {
+                'id': user.key.id(),
+                'last_name': user.last_name,
+                'first_name': user.first_name,
+                'email': user.email
+            }
+            user.key.delete()
+            return jsonify(response)
+
+    return jsonify({'error': {'status': 401, 'message': 'unauthorized'}}), 401
