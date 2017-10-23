85e7d44f594b42a39f631d234642a202d29a26d7
marvinjason <marvinjason.sy@gmail.com>
2017-10-23 14:52:15 +0800

    Fix sort message

--- app/static/controllers.py
+++ app/static/controllers.py
@@ -175,22 +175,20 @@ def index(id):
         total_count = 0
 
         if filter in CATEGORIES[:2]:
-            data = MessagePointer.query(ndb.AND(MessagePointer.to_recipient == session.user, MessagePointer.category == filter)).order(-MessagePointer.datetime_created).fetch(count, offset=offset)
+            data = MessagePointer.query(ndb.AND(MessagePointer.to_recipient == session.user, MessagePointer.category == filter))
+            data = data.order(MessagePointer.datetime_created).fetch(count, offset=offset)
             total_count = MessagePointer.query(MessagePointer.to_recipient == session.user).count()
-
         elif filter in CATEGORIES[2:]:
-            messages = Message.query(Message.from_recipient == session.user).order(-Message.datetime_created).fetch(count, offset=offset)
-            
+            messages = Message.query(Message.from_recipient == session.user).order(Message.datetime_created).fetch(count, offset=offset)
             for m in messages:
                 pointer = MessagePointer.query(ndb.AND(MessagePointer.message == m.key, MessagePointer.category == filter)).get()
                 data += [pointer] if pointer else []
-
+                
             messages = Message.query(Message.from_recipient == session.user).fetch()
-            
+
             for m in messages:
                 pointer = MessagePointer.query(ndb.AND(MessagePointer.message == m.key, MessagePointer.category == filter)).get()
                 total_count += 1 if pointer else 0
-
         else:
             return get_status_code(400)
 
